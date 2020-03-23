55818ceaa416f0231f24f6e77e3e45cdc7beb994
Cortlyn Rosser <coryrosser@gmail.com>
2020-03-23 16:05:20 +0000

    Automatically backed up by Learn

--- lib/aoh_example.rb
+++ lib/aoh_example.rb
@@ -24,9 +24,15 @@ def literal_aoh
 end
 
 def aoh_lookup(aoh, row, key)
+  
+  return aoh[row][key]
+  
 end
 
 def aoh_update(aoh, row, key, new_value)
+  
+  aoh[row][key] = new_value
+  
   # Update the AoH data at row and key to have the value of new_value
   # Return the updated AoH
 end
